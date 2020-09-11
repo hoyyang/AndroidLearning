6fb7e7d3bc803181a6aa4eca0afc87ef1eb4ad6b
Haoyu Yang <hyan0050@student.monash.edu>
2020-09-11 14:42:04 +0800

    Add WebView with loading URL

--- WebView Apps/AppForWebView/app/src/main/java/com/example/appforwebview/MainActivity.java
+++ WebView Apps/AppForWebView/app/src/main/java/com/example/appforwebview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appforwebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.wv_quiz_question);
+        webView.loadUrl("https://quiz.tryinteract.com/#/5f5afd407c8b8c00146c2b7e?method=iframe");
     }
 }
