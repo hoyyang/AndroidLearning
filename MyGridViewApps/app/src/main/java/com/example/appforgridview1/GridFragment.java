18316921735ed2bb39c1d2a99eb4d09dcff5729f
Haoyu Yang <hyan0050@student.monash.edu>
2020-09-13 09:41:41 +0800

    Add new fragment

---
+++ MyGridViewApps/app/src/main/java/com/example/appforgridview1/GridFragment.java
@@ -0,0 +1,29 @@
+package com.example.appforgridview1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class GridFragment extends Fragment {
+    private View view;
+
+    public GridFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the View for this fragment
+        this.view = inflater.inflate(R.layout.fragment_grid, container, false);
+
+
+
+        return this.view;
+    }
+}
--- MyGridViewApps/app/src/main/java/com/example/appforgridview1/MainActivity.java
+++ MyGridViewApps/app/src/main/java/com/example/appforgridview1/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.appforgridview1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.button = findViewById(R.id.btn_open);
+    }
+
+    public void openFragment(View view){
+        this.button.setVisibility(View.INVISIBLE);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_fragments, new GridFragment(), "grid").commit();
     }
 }
